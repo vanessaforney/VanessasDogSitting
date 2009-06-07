743ac762619043bd9986f4d226a32464e10afd4d
michaelforney <michael@obberon.com>
2009-06-06 22:08:09 -0700

    more theme stuff

--- menu.php
+++ menu.php
@@ -3,14 +3,14 @@
         <?php
             function generate_menu()
             {
-                global $main_pages, $index_path;
+                global $main_pages, $index_path, $image_dir;
                 foreach($main_pages as $page_id => $page_title)
                 {
                     if ($page_id == "not_found")
                     {
                         continue;
                     }
-                    echo "<div><li><a href=\"" . $index_path . "/" . $page_id . "\"><img src=\"" . $image_dir . "/" . $page_title . ".png\" /></a></li></div>";
+                    echo "<div><li><a href=\"" . $index_path . "/" . $page_id . "\"><img src=\"" . $image_dir . "/" . $page_id . ".png\" /></a></li></div>";
                 }
             }
             generate_menu();
