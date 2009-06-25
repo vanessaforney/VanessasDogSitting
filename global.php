7fbfa9e6390cdb1668f8cbe12241a2e7ccdb14ba
Michael Forney <michael@obberon.com>
2009-06-25 12:51:26 -0700

    Configured for new domain

--- global.php
+++ global.php
@@ -1,16 +1,16 @@
 <?php
     require_once("texy/texy.php");
-    
+
     $main_pages = array("home" => "Home", "aboutme" => "About Me", "satisfied_clients" => "Satisfied Clients", "calender" => "Calender", "rates" => "Rates", "contact_info" => "Contact Info", "not_found" => "Not Found");
     $style = "default";
-    $site_path = "/vanessasdogsitting.com";
-    $index_path = $site_path . "/ds.php";
+    $site_path = "";
+    $index_path = $site_path . "";
 
     $texy = new Texy();
 
-    if ($_GET["cssdebug"] == "true")
+    if (isset($_GET["cssdebug"]) && $_GET["cssdebug"] == "true")
     {
-        $css = "C:/Users/Michael/Development/vanessa_dogsitting/style/default/default.css";
+        $css = "file:///home/michael/scm/vanessa_dogsitting/style/default/default.css";
     }
     else
     {
@@ -30,7 +30,7 @@
     $path = explode("/", $path_info);
     array_shift($path);
 
-    
+
     if (array_key_exists($path[0], $main_pages))
     {
         $page_id = array_pop($path);
@@ -49,10 +49,10 @@
         $page_title = $main_pages[$page_id];
         $page_path = "";
     }
-    
+
     function get_content()
     {
-        
+
         global $texy, $main_pages, $path, $page_id, $page_title, $page_path;
         $texy_file = "pages/" . $page_path . "/" . $page_id . ".texy";
         if (file_exists($texy_file))
@@ -60,19 +60,5 @@
             $text = file_get_contents($texy_file);
             echo $texy->process($text);
         }
-    }
-
-    function generate_menu()
-    {
-        global $texy, $main_pages, $index_path, $image_dir;
-        $text = "/---div";
-        foreach($main_pages as $page_id => $page_title)
-        {
-            $text .= "/---div
-            - [* " . $image_dir . "/" . $page_id . ".png *]:[" . $index_path . "/" . $page_id . "]
-            \---";
-        }
-        $text .= "\\---div";
-        echo $text->process($text);
     }
 ?>
--- menu.php
+++ menu.php
@@ -1,22 +1,22 @@
 <div id="menu">
 	<ul>
         <?php
-//             function generate_menu()
-//             {
-//                 global $main_pages, $index_path, $image_dir;
-//                 foreach($main_pages as $page_id => $page_title)
-//                 {
-//                     if ($page_id == "not_found")
-//                     {
-//                         continue;
-//                     }
-//                     echo "<div><li><a href=\"" . $index_path . "/" . $page_id . "\"><img src=\"" . $image_dir . "/" . $page_id . ".png\" /></a></li></div>";
-//                 }
-//             }
+            function generate_menu()
+            {
+                global $main_pages, $index_path, $image_dir;
+                foreach($main_pages as $page_id => $page_title)
+                {
+                    if ($page_id == "not_found")
+                    {
+                        continue;
+                    }
+                    $image = $image_dir . "/" . $page_id . ".png";
+                    $image_hover = $image_dir . "/" . $page_id . "_hover.png";
+                    echo "<div class=\"li-outside\"><li><a href=\"" . $index_path . "/" . $page_id . "\"><img src=\"" . $image . "\" onmouseover=\"this.src='" . $image_hover . "'\" onmouseout=\"this.src='" . $image . "'\"
+                    onload=\"var i=new Image();i.src='" . $image_hover . "';if(typeof preload=='undefined')preload=new Array();preload[preload.length]=i;this.onload=''\" /></a></li></div>";
+                }
+            }
             generate_menu();
         ?>
 	</ul>
-    <div class="menu-below">
-        test
-    </div>
 </div>
